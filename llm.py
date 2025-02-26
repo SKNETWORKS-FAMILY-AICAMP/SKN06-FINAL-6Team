42e689e023dc74b91a1b779aa8beef20c435619a
Sujeon-j <en123460@naver.com>
2025-02-26 17:04:25 +0900

    메모리 수정

--- llm.py
+++ llm.py
@@ -1,28 +1,49 @@
-import pandas as pd
 import sqlite3
+import pandas as pd
+from typing import List
+from pydantic import BaseModel, Field
 
-from langchain_community.document_loaders import DataFrameLoader
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.document_loaders import DataFrameLoader
 from langchain_community.vectorstores import FAISS
-from langchain.retrievers import EnsembleRetriever
 from langchain_community.retrievers import BM25Retriever
+from langchain.retrievers import EnsembleRetriever
+from langchain_core.messages import BaseMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from langchain_core.runnables import ConfigurableFieldSpec
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.output_parsers import StrOutputParser
-from langchain.memory import ConversationBufferMemory
+from langchain_core.chat_history import BaseChatMessageHistory
+from operator import itemgetter
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-### 임베딩 모델
+class InMemoryHistory(BaseChatMessageHistory, BaseModel):
+    """인메모리 채팅 히스토리 클래스"""
+    messages: List[BaseMessage] = Field(default_factory=list)
+
+    def add_messages(self, messages: List[BaseMessage]) -> None:
+        """Add a list of messages to the store"""
+        self.messages.extend(messages)
+
+    def clear(self) -> None:
+        self.messages = []
+
+## 임베딩 모델
 embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 
-# 모델
+## 모델
 model = ChatOpenAI(model="gpt-4o-mini")
 
-### 메모리
-memory = ConversationBufferMemory(llm=model, max_token_limit=200, return_messages=True, memory_key="history")
+# 메모리 정의
+store = {}
+def get_session_history(user_id: str, history_id: str) -> BaseChatMessageHistory:
+    if (user_id, history_id) not in store:
+        store[(user_id, history_id)] = InMemoryHistory()
+    print(f"현재 저장된 히스토리: {store}") # 디버깅용
+    return store[(user_id, history_id)]
 
 def load(case):
     if case == "func":
@@ -112,22 +133,18 @@ def mkch():
             ("ai", """
             너는 사용자의 질문(question)에 맞는 요리를 알려주는 ai야.
 
-            요리 소개할 때 이름을 언급한 뒤, 한 줄 정도 간단한 요리 소개를 하고 재료를 알려줘.
-            요리 이름은 이름에서 만든 사람을 알 수 있다면 요리사도 같이 알려줘.
-            사용자가 네가 추천해준 요리 안에서 요리를 선택하면 사진 혹은 영상과 함께 레시피를 알려줘.
-            만약 해당 요리의 사진, 영상이 모두 없으면, 알려주지말고 하나라도 있으면 같이 첨부해서 알려줘.
-            요리는 기본적으로 세 가지 추천주고, 사용자가 특정 요리 하나를 질문한 경우에만 해당하는 요리 한가지만 알려줘.
-            사용자의 질문의 답을 context에서 적절한 요리를 찾지 못하면 추가 정보를 좀 더 수집한 뒤 답변해.
-            답변을 context에서 찾을 수 없으면 답변을 생성하지 말고 모른다고 대답해.
+            요리 소개할 때 요리 이름을 언급한 뒤, 한 줄 정도 간단한 요리 소개를 하고 재료, 사진을 알려줘.
+            요리 이름은 이름에서 만든 사람을 알 수 있다면 요리사도 같이 알려주고, 사진은 context에서 해당하는 요리의 사진을 알려주면 돼. 없으면 알려주지마.
+            요리는 기본적으로 3가지 추천 해주고, 사용자가 특정 요리 하나를 질문한 경우에만 해당하는 요리 1가지만 알려줘.
+            요리 추천 기준은 사용자가 재료를 입력했을 경우, 해당 재료는 많은 순으로 먼저 정렬하고, 우선 순위가 같은 요리에 대해서는 부가적인 재료가 적은 순으로 재정렬해줘.
+            레시피는 요약하지말고, 있는 그대로 순서대로 알려줘.
+            레시피 알려줄 때 영상도 같이 알려줘야해. 만약 영상이 없으면 알려주지마. context에 네가 알려줄 요리의 영상이이 있을 때만 같이 첨부해서 알려줘.
+            사용자의 질문의 답을 context에서 적절한 요리를 찾지 못하면 필요에 따라 추가 정보를 좀 더 수집한 뒤 답변하고, 그럼에도 답변할 내용을 context에서 찾을 수 없으면 답변을 생성하지 말고 모른다고 대답해.
     {context}"""),
             MessagesPlaceholder(variable_name="history", optional=True),
             ("human", "{question}"),
         ]
     prompt_template = ChatPromptTemplate(messages)
-
-    # 메모리
-    def load_history(input):
-        return memory.load_memory_variables({})["history"]
 
     # retriever 로드 => 추후 함수 선택 코드 넣어야 함
     rbm25_retr, rfais_retr = ref() # 냉장고를 부탁해
@@ -136,18 +153,26 @@ def mkch():
 
     retriever = EnsembleRetriever(retrievers=[rbm25_retr, rfais_retr, fbm25_retr, ffais_retr, mbm25_retr, mfais_retr],) # weights=[0.25, 0.25, 0.25, 0.25],) # weight: retriever 별 가중치 조절 가능
 
-    # Chain 구성 retriever(관련 문서 조회) -> prompt_template(prompt 생성) model(정답) -> output parser
-    chain = RunnableLambda(lambda x:x['question']) | {"context": retriever, "question":RunnablePassthrough() , "history": RunnableLambda(load_history)}  | prompt_template | model | StrOutputParser()
+    # chatting Chain 구성 retriever(관련 문서 조회) -> prompt_template(prompt 생성) model(정답) -> output parser
+    chatting = {"context": itemgetter("question") | retriever, "question": itemgetter("question"), "history": itemgetter("history")} | prompt_template | model | StrOutputParser()
+
+    # 메모리 결합 체인인
+    chain = RunnableWithMessageHistory(
+        chatting, get_session_history=get_session_history, input_messages_key="question", history_messages_key="history",
+        history_factory_config=[
+            ConfigurableFieldSpec(id="user_id", annotation=str, name="User ID", description="사용자 id(Unique)", default="", is_shared=True),
+            ConfigurableFieldSpec(id="history_id", annotation=str, name="History ID", description="대화 기록 id(Unique)", default="", is_shared=True),
+        ]
+    )
     return chain
 
-def chat():
+def chat(user_id, history_id):
     chain = mkch()
     while True:
         query = input("메시지 입력 > ")
         if query == "종료":
             break
-        res = chain.invoke({"question": query})
-        memory.save_context(inputs={"human": query}, outputs={"ai":res})
+        res = chain.invoke({"question": query}, config={"configurable": {"user_id": user_id, "history_id": history_id}})
         print(res)
 
 chat()
