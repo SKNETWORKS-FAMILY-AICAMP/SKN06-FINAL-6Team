8bf401e355c1bf54a1c8fbda5977ab7f4d05c669
ChaeYeon Kang <145631063+codus090880@users.noreply.github.com>
2025-03-14 15:13:44 +0900

    Delete fp folder

--- fp/cook/account/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import AdminLog
-
-@admin.register(AdminLog)
-class AdminLogAdmin(admin.ModelAdmin):
-    list_display = ('action_type', 'user', 'review', 'comment', 'created_at')
-    list_filter = ('action_type', 'created_at')
-    search_fields = ('user__nickname', 'review__review_text')
