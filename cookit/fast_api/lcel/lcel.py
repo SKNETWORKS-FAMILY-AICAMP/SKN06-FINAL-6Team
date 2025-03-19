8e08130fe091cb3e934998fe2a8f7017f7ec2b85
Ubuntu <ubuntu@ip-172-31-10-181.ap-northeast-2.compute.internal>
2025-03-19 16:58:57 +0000

    .

--- cookit/fast_api/lcel/lcel.py
+++ cookit/fast_api/lcel/lcel.py
@@ -1,5 +1,5 @@
+import importlib
 from textwrap import dedent
-from operator import itemgetter
 from pydantic import BaseModel, Field
 from typing_extensions import Literal
 
@@ -61,68 +61,12 @@ def intent(query):
     chk_intent_chain = intent_prompt | intent_result
     return chk_intent_chain
 
-# 2-1. 기본 질문일 경우
-@chain
-def normal(query):
-    # i. retriever에 쿼리 전달 및 context 저장(format_docs에서 실행)
-    messages = [(("system", dedent(
-        """
-        너는 사용자의 질문(question)에 맞는 요리를 추천해주는 ai야.
-        다음 조건에 맞춰서 답변해.
-        
-        # 조건
-        1. context에서 답변을 찾을 수 없으면 답변을 만들지 말고 `모르겠습니다.`라고 대답한다.
-        
-        2. 검색 결과를 알려줄 때 요리를 3가지 알려준다.
-        2-1. 요리를 알려줄 때는 요리 이름, 요리 한 줄 소개, 요리 재료, 사진을 알려준다. 이외 정보는 언급하지 않는다.
-        2-2. 사용자가 요리에 포함되어야하는 재료를 여러 개 입력했을 경우, 사용자가 언급한 재료가 많이 있는 순으로 먼저 정렬하고, 우선 순위가 같은 요리에 대해서는 부가적인 재료가 적은 순으로 정렬한 후 추천한다.
-        2-3. 사용자가 포함하지 말아야할 재료나 도구 등을 언급하면 그것은 포함하지 않는 요리만 추천한다.
-        2-4. 사진은 요리정보의 `img` key에 있다. 반드시 답변하는 요리와 같은 id의 `img` key의 value를 알려준다. 다른 요리의 것은 절대 알려주면 안된다. 답변하는 요리에 `img`가 없거나 값이 없으면(빈문자열이면) 사진은 사용자에게 제공하지 않는다.
-        
-        3. 사용자가 요리 이름을 언급하며 레시피를 알려달라고 요청하면, 검색 결과 내용을 말로 풀어서 한 가지 요리 정보만 전달한다.
-        3-1. 요리 이름, 재료, 레시피, 사진, 영상을 제공한다.
-        3-2. 이외 사항은 2-1, 2-2. 2-3, 2-4 조건에 맞게 대답한다.
-        # context
-        {context}
-        """))), 
-        MessagesPlaceholder(variable_name="history", optional=True),
-        ("human", "{question}")]
-    prompt_template = ChatPromptTemplate(messages)
-
-    nchain = {"context": itemgetter("question") | retriever | format_docs, "question": itemgetter("question"), "history": itemgetter("history")} | prompt_template | model | StrOutputParser()
-    # ii. llm에 retrieving 결과 + history 전달
-    return nchain
-
-# 2-2. 이전 질문의 파생 질문일 경우
-@chain
-def derived(query):
-    # i. history + 이전 질문 context llm에 전달
-    messages = [(("system", dedent(
-        """
-        너는 사용자의 질문(question)에 맞는 요리를 추천해주는 ai야.
-        다음 조건에 맞춰서 답변해.
-        
-        # 조건
-        1. 사용자가 이전에 추천해준 요리 목록에서 요리를 고르면 `history`를 참고해 사용자가 요구하는 요리의 레시피와 영상을 `content`에서 찾아 알려준다.
-        1-1. `_collection_name` key의 value가 `funs`, `ref 중 하나인 경우 요리 이름은 변형하지 않고 그대로 대답한다.
-        1-2. 영상은 요리정보의 `video` key에 있다. 반드시 답변하는 요리와 같은 id의 `video` key의 value를 알려준다. 다른 요리의 것은 절대 알려주면 안된다. 답변하는 요리에 `video`가 없거나 값이 없으면(빈문자열이면) 제공하지 않는다.
-
-        2. 이전 질문에서 대체할 수 있는 재료 등의 질문이 들어오면 질문에 맞는 대답을 알려준다.
-        # content
-        {content}
-        # context
-        {context}
-        """))),
-        MessagesPlaceholder(variable_name="history", optional=True),
-        ("human", "{question}")]
-    prompt_template = ChatPromptTemplate(messages)
-    dchain = {"question": itemgetter("question"), "history": itemgetter("history"), "content": itemgetter("content"), "context": itemgetter("question") | retriever | format_docs} | prompt_template | model | StrOutputParser()
-    return dchain
-
 def mkch(isref=True, isfun=True, isman=True):
-    global retriever
-    retriever = load_retriever(isref, isfun, isman)
-    base_chain = RunnableLambda(lambda x: (lambda chain:chain.invoke({**x, "content": contents if chain == derived else []}))(select_chain(x)))
+    def chain_runner(x):
+        selected_chain = select_chain(x)
+        selection = importlib.import_module("utils.agents")
+        return selected_chain.invoke(x) if selected_chain == getattr(selection, "normal") else selected_chain.invoke({**x, "content": contents})
+    base_chain = RunnableLambda(chain_runner)
     mkchain = RunnableWithMessageHistory(
         base_chain, get_session_history=get_session_history, input_messages_key="question", history_messages_key="history",
         history_factory_config=[
@@ -135,7 +79,6 @@ def mkch(isref=True, isfun=True, isman=True):
 def select_chain(x):
     """intent 분석 결과에 따라 실행할 체인을 선택"""
     history = x['history'][:-6]
+    selection = importlib.import_module("utils.agents")
     chk = intent.invoke({"query": x["question"], "history": history})
-    if chk.flag == "continue":
-        return derived
-    return normal
+    return getattr(selection, "derived") if chk.flag == "continue" else getattr(selection, "normal")
--- cookit/fast_api/utils/agents.py
+++ cookit/fast_api/utils/agents.py
@@ -1,9 +1,21 @@
-from langchain.tools import Tool
+import importlib
+from textwrap import dedent
+
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langchain_core.runnables import chain
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_community.tools import TavilySearchResults
+from langchain.tools import Tool
 
 from dotenv import load_dotenv
 
 load_dotenv()
+model = ChatOpenAI(model="gpt-4o-mini")
+retrievers = importlib.import_module('lcel.lcel')
+if hasattr(retrievers, "retriever"):
+    retriever = getattr(retrievers, "retriever")
 
 tavily_client = TavilySearchResults(max_results=1)
 
@@ -17,3 +29,60 @@ def search_tavily_recipe(query):
     return "검색 결과 없음"
 
 tavily_search = Tool(name="Tavily Recipe Search", func=search_tavily_recipe, description="Tavily 검색을 이용해 레시피 관련 첫 번째 블로그 URL을 가져오는 도구")
+
+@chain
+def normal(query):
+    # i. retriever에 쿼리 전달 및 context 저장(format_docs에서 실행)
+    messages = [(("system", dedent(
+        """
+        너는 사용자의 질문(question)에 맞는 요리를 추천해주는 ai야.
+        다음 조건에 맞춰서 답변해.
+        
+        # 조건
+        1. query를 사용자가 요구한 결과가 출력될 수 있게 변형해 `menu_search` tool을 실행한다. menu에서 답변을 찾을 수 없으면 답변을 만들지 말고 `모르겠습니다.`라고 대답한다.
+        
+        2. 검색 결과를 알려줄 때 요리를 3가지 알려준다.
+        2-1. 요리를 알려줄 때는 요리 이름, 요리 한 줄 소개, 요리 재료, 사진을 알려준다. 이외 정보는 언급하지 않는다.
+        2-2. 사용자가 요리에 포함되어야하는 재료를 여러 개 입력했을 경우, 사용자가 언급한 재료가 많이 있는 순으로 먼저 정렬하고, 우선 순위가 같은 요리에 대해서는 부가적인 재료가 적은 순으로 정렬한 후 추천한다.
+        2-3. 사용자가 포함하지 말아야할 재료나 도구 등을 언급하면 그것은 포함하지 않는 요리만 추천한다.
+        2-4. 사진은 요리정보의 `img` key에 있다. 반드시 답변하는 요리와 같은 id의 `img` key의 value를 알려준다. 다른 요리의 것은 절대 알려주면 안된다. 답변하는 요리에 `img`가 없거나 값이 없으면(빈문자열이면) 사진은 사용자에게 제공하지 않는다.
+        
+        3. 사용자가 요리 이름을 언급하며 레시피를 알려달라고 요청하면, 검색 결과 내용을 말로 풀어서 한 가지 요리 정보만 전달한다.
+        3-1. 요리 이름, 재료, 레시피, 사진, 영상을 제공한다.
+        3-2. 이외 사항은 2-1, 2-2, 2-3, 2-4 조건에 맞게 대답한다.
+        """))), 
+        MessagesPlaceholder(variable_name="history", optional=True),
+        ("human", "{question}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad")]
+    prompt_template = ChatPromptTemplate(messages)
+
+    tools = [menu_search]
+    agent = create_tool_calling_agent(model, tools, prompt_template)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=False)
+    return agent_executor
+
+@chain
+def derived(query):
+    # i. history + 이전 질문 context llm에 전달
+    messages = [(("system", dedent(
+        """
+        너는 사용자의 질문(question)에 맞는 요리를 추천해주는 ai야.
+        다음 조건에 맞춰서 답변해.
+        
+        # 조건
+        1. 사용자가 이전에 추천해준 요리 목록에서 요리를 고르면 `history`와 `content`를 참고해 해당 요리의 레시피와 영상을 알려준다.
+        1-1. `_collection_name` key의 value가 `funs`, `ref 중 하나인 경우 요리 이름은 변형하지 않고 그대로 대답한다.
+        1-2. 영상은 요리정보의 `video` key에 있다. 반드시 답변하는 요리와 같은 id의 `video` key의 value를 알려준다. 다른 요리의 것은 절대 알려주면 안된다. 답변하는 요리에 `video`가 없거나 값이 없으면(빈문자열이면) 제공하지 않는다.
+        
+        2. 이전 질문에서 대체할 수 있는 재료 등의 질문이 들어오면 질문에 맞는 대답을 알려준다.
+        """))),
+        MessagesPlaceholder(variable_name="history", optional=True),
+        MessagesPlaceholder(variable_name="content", optional=True),
+        ("human", "{question}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad")]
+    prompt_template = ChatPromptTemplate(messages)
+    
+    tools = [menu_search]
+    agent = create_tool_calling_agent(model, tools, prompt_template)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=False)
+    return agent_executor
